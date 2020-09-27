3c400f3bba36c012e1e31f112b89d5483a39bcd3
Navishka Kularathna <navishka.randika@gmail.com>
2020-09-27 17:02:02 +0530

    Update customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,14 @@ public class CustomerRestController {
 		
 		//In case if they pass an ID in JSON
 		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	@PutMapping("/customer")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 		
 		customerService.saveCustomer(theCustomer);
 		
